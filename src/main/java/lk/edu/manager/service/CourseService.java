9ac9d0aef9605450307c015519579c237d7e4110
ishara <isharauditha5@gmail.com>
2025-01-16 00:47:51 +0530

    add course controller

--- src/main/java/lk/edu/manager/service/CourseService.java
+++ src/main/java/lk/edu/manager/service/CourseService.java
@@ -5,6 +5,7 @@ import lk.edu.manager.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
