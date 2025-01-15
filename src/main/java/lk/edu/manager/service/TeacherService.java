e1ae98158989ce431f016f7177df4e50ffeb1699
ishara <isharauditha5@gmail.com>
2025-01-15 23:47:27 +0530

    add Teacher service

---
+++ src/main/java/lk/edu/manager/service/TeacherService.java
@@ -0,0 +1,39 @@
+package lk.edu.manager.service;
+
+import lk.edu.manager.entities.Teacher;
+import lk.edu.manager.repository.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TeacherService {
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    public List<Teacher> getAllTeachers() {
+        return teacherRepository.findAll();
+    }
+
+    public Optional<Teacher> getTeacherById(Long id) {
+        return teacherRepository.findById(id);
+    }
+
+    public Teacher createTeacher(Teacher teacher) {
+        return teacherRepository.save(teacher);
+    }
+
+    public Teacher updateTeacher(Long id, Teacher teacherDetails) {
+        Teacher teacher = teacherRepository.findById(id).orElseThrow();
+        teacher.setFirstName(teacherDetails.getFirstName());
+        teacher.setLastName(teacherDetails.getLastName());
+        teacher.setEmail(teacherDetails.getEmail());
+        return teacherRepository.save(teacher);
+    }
+
+    public void deleteTeacher(Long id) {
+        teacherRepository.deleteById(id);
+    }
+}
