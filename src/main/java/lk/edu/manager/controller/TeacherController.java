8b0a797601398c802e0bc3ca3ea551f04334ff02
ishara <isharauditha5@gmail.com>
2025-01-15 23:52:43 +0530

    add Teacher controller

---
+++ src/main/java/lk/edu/manager/controller/TeacherController.java
@@ -0,0 +1,47 @@
+package lk.edu.manager.controller;
+
+import lk.edu.manager.entity.Teacher;
+import lk.edu.manager.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/teachers")
+public class TeacherController {
+    @Autowired
+    private TeacherService teacherService;
+
+    @GetMapping
+    public List<Teacher> getAllTeachers() {
+        return teacherService.getAllTeachers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Teacher> getTeacherById(@PathVariable Long id) {
+        Optional<Teacher> teacher = teacherService.getTeacherById(id);
+        return teacher.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Teacher> createTeacher(@RequestBody Teacher teacher) {
+        Teacher savedTeacher = teacherService.createTeacher(teacher);
+        return new ResponseEntity<>(savedTeacher, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Teacher> updateTeacher(@PathVariable Long id, @RequestBody Teacher teacherDetails) {
+        Teacher updatedTeacher = teacherService.updateTeacher(id, teacherDetails);
+        return ResponseEntity.ok(updatedTeacher);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTeacher(@PathVariable Long id) {
+        teacherService.deleteTeacher(id);
+        return ResponseEntity.noContent().build();
+    }
+}
